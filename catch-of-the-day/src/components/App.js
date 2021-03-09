1d7b5a27e5fbcf0d058871c2611035421208bc39
ibrianfrancisco <ibrianfrancisco@gmail.com>
2021-03-08 17:14:53 -0800

    finished video 21

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -53,6 +53,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    // 1. take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. update the state
+    fishes[key] = null;
+    // 3. set state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -63,6 +72,15 @@ class App extends React.Component {
     // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    // 1. Take a copy of the state
+    const order = { ...this.state.order };
+    // 2. update the state
+    delete order[key];
+    // 3. set that to state
     this.setState({ order });
   };
 
@@ -82,10 +100,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
